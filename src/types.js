91870f29de84d7fcd1422150d794f0a144fcd1b1
Mike Grabowski <grabbou@gmail.com>
2016-09-24 21:04:49 +0100

    Document type

--- src/types.js
+++ src/types.js
@@ -8,12 +8,26 @@
  * @flow
  */
 
+// JS version of FBNativeAd
 export type NativeAd = {
+  // URL to ad icon image
   icon: ?string,
+
+  // URL to ad cover image creative
   coverImage: ?string,
+
+  // Ad title
   title: ?string,
+
+  // Ad subtitle
   subtitle: ?string,
+
+  // Body text, usually a longer description of the ad.
   description: ?string,
+
+  // Call to action phrase of the ad, for example "Install Now".
   callToActionText: ?string,
+
+  // Ad social context, for example "Over half a million users".
   socialContext: ?string,
 }
