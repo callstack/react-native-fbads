0324ea4cc5701b4453d64d62188a140047b075f5
Jakub Stasiak <kubafr2@gmail.com>
2016-09-30 16:51:38 +0200

    Bloking possibility of opening multiple interstitials

--- src/android/src/main/java/io/callstack/react/fbads/InterstitialAdManager.java
+++ src/android/src/main/java/io/callstack/react/fbads/InterstitialAdManager.java
@@ -3,47 +3,32 @@ package io.callstack.react.fbads;
 import com.facebook.ads.AbstractAdListener;
 import com.facebook.ads.Ad;
 import com.facebook.ads.AdError;
+import com.facebook.ads.AdListener;
 import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
+import com.facebook.react.bridge.AssertionException;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-public class InterstitialAdManager extends ReactContextBaseJavaModule {
+public class InterstitialAdManager extends ReactContextBaseJavaModule implements InterstitialAdListener {
 
   public InterstitialAdManager(ReactApplicationContext reactContext) {
     super(reactContext);
   }
 
   @ReactMethod
-  public void showAd(String placementId, final Promise promise) {
+  public void showAd(String placementId, Promise p) {
+    if(promise != null) {
+      return;
+    }
+
     ReactApplicationContext reactContext = this.getReactApplicationContext();
 
-    final InterstitialAd interstitial = new InterstitialAd(reactContext, placementId);
-    interstitial.setAdListener(new AbstractAdListener() {
-
-      @Override
-      public void onAdLoaded(Ad ad) {
-        if (ad == interstitial) {
-          interstitial.show();
-        }
-      }
-
-      @Override
-      public void onError(Ad ad, AdError error) {
-        promise.reject("" + error.getErrorCode(), error.getErrorMessage());
-      }
-
-      @Override
-      public void onAdClicked(Ad ad) {
-          promise.resolve(true);
-      }
-
-      @Override
-      public void onInterstitialDismissed(Ad ad) {
-        promise.resolve(false);
-      }
-    });
+    promise = p;
+    interstitial = new InterstitialAd(reactContext, placementId);
+    interstitial.setAdListener(this);
     interstitial.loadAd();
   }
 
@@ -51,4 +36,42 @@ public class InterstitialAdManager extends ReactContextBaseJavaModule {
   public String getName() {
     return "CTKInterstitialAdManager";
   }
+
+  @Override
+  public void onError(Ad ad, AdError adError) {
+    promise.reject("" + adError.getErrorCode(), adError.getErrorMessage());
+  }
+
+  @Override
+  public void onAdLoaded(Ad ad) {
+    if (ad == interstitial) {
+      interstitial.show();
+    }
+  }
+
+  @Override
+  public void onAdClicked(Ad ad) {
+    resolveAndCleanUp(true);
+  }
+
+  @Override
+  public void onInterstitialDismissed(Ad ad) {
+    resolveAndCleanUp(false);
+  }
+
+  @Override
+  public void onInterstitialDisplayed(Ad ad) {
+
+  }
+
+  private void resolveAndCleanUp(boolean result) {
+    if(promise != null) {
+      promise.resolve(result);
+    }
+
+    promise = null;
+  }
+
+  private Promise promise;
+  private InterstitialAd interstitial;
 }
