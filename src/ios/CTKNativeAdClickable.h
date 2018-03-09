a1e16873a8836111245f5f2e21a1995f181a0d1e
Philip Heinser <philipheinser@me.com>
2018-03-09 13:39:30 +0100

    clickable and adChoices for ios

---
+++ src/ios/CTKNativeAdClickable.h
@@ -0,0 +1,14 @@
+//
+//  CTKNativeAdClickable.h
+//  rn-fbads
+//
+//  Created by Philip Heinser on 09.03.18.
+//  Copyright © 2018 callstack. All rights reserved.
+//
+
+#import <React/RCTView.h>
+#import <React/RCTComponent.h>
+
+@interface CTKNativeAdClickable : RCTView
+
+@end
---
+++ src/ios/CTKNativeAdClickableManager.h
@@ -0,0 +1,14 @@
+//
+//  CTKNativeAdClickableManager.h
+//  rn-fbads
+//
+//  Created by Philip Heinser on 09.03.18.
+//  Copyright © 2018 callstack. All rights reserved.
+//
+
+#import <React/RCTView.h>
+#import <React/RCTViewManager.h>
+
+@interface CTKNativeAdClickableManager : RCTViewManager
+
+@end
