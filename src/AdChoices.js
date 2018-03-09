988f3eb38fe6dc57d54486d470df219a1cf2af76
Philip Heinser <philipheinser@me.com>
2018-03-09 13:46:26 +0100

    fix ios no choice component

--- src/AdChoices.js
+++ src/AdChoices.js
@@ -2,20 +2,22 @@ import React, { Component, PropTypes } from 'react';
 import { View, requireNativeComponent } from 'react-native';
 
 var cfg = {
-    nativeOnly: {
-        shouldNotifyLoadEvents: true
-    },
-    propTypes: {
-        // nativeAdView: PropTypes.number,
-        ...View.propTypes
-    }
+  nativeOnly: {
+    shouldNotifyLoadEvents: true,
+  },
+  propTypes: {
+    // nativeAdView: PropTypes.number,
+    ...View.propTypes,
+  },
 };
 
-const AdChoices = requireNativeComponent('CTKAdChoice', null);
-
+const AdChoices =
+  Platform.OS === 'android'
+    ? requireNativeComponent('CTKAdChoice', null)
+    : View;
 
 const AdChoicesComponent = props => {
-    return <AdChoices {...props} />;
-}
+  return <AdChoices {...props} />;
+};
 
 export default AdChoicesComponent;
