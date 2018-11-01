e0092bb5e8485786c64603756afb165162d63a67
surajtiwari <surajtiwari020@gmail.com>
2018-11-01 20:39:55 +0530

    added default adchoice location.

--- src/native-ads/AdChoicesManager.tsx
+++ src/native-ads/AdChoicesManager.tsx
@@ -21,7 +21,7 @@ interface AdChoicesProps {
 // tslint:disable-next-line:variable-name
 const NativeAdChoicesView = requireNativeComponent<
   AdChoicesProps & { placementId: string }
->('AdChoicesView');
+  >('AdChoicesView');
 
 type AdChoiceLocation = 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
 
@@ -36,7 +36,11 @@ export default class AdChoicesView extends React.Component<AdChoicesProps> {
       <AdChoicesViewContext.Consumer>
         {(placementId: AdChoicesViewContextValueType) => (
           <NativeAdChoicesView
-            style={[styles.adChoice, this.props.style]}
+            style={[
+              styles.adChoice,
+              this.props.style,
+              this.getPositionStyle(this.props.location),
+            ]}
             placementId={placementId}
             location={this.props.location}
             expandable={this.props.expandable}
@@ -44,6 +48,21 @@ export default class AdChoicesView extends React.Component<AdChoicesProps> {
         )}
       </AdChoicesViewContext.Consumer>
     );
+  }
+
+  getPositionStyle = (position: AdChoiceLocation) => {
+    switch (position) {
+      case 'topLeft':
+        return styles.topLeft;
+      case 'topRight':
+        return styles.topRight;
+      case 'bottomLeft':
+        return styles.bottomLeft;
+      case 'bottomRight':
+        return styles.bottomRight;
+      default:
+        throw new Error(`Unsupported position ${position}`);
+    }
   }
 }
 
@@ -60,5 +79,22 @@ const styles = StyleSheet.create({
         height: 22,
       },
     }),
+    position: 'absolute',
+  },
+  topLeft: {
+    left: 0,
+    top: 0,
+  },
+  topRight: {
+    top: 0,
+    right: 0,
+  },
+  bottomLeft: {
+    left: 0,
+    bottom: 0,
+  },
+  bottomRight: {
+    bottom: 0,
+    right: 0,
   },
 });
