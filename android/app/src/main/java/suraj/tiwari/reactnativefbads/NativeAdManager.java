b4b4f19cc183952b53fe2de88073a7e537a35365
Jeungmin Oh <tangza@gmail.com>
2018-11-13 14:48:24 +0900

    Add missing `java.util.Locale;` for NativeAdManager.java

--- android/app/src/main/java/suraj/tiwari/reactnativefbads/NativeAdManager.java
+++ android/app/src/main/java/suraj/tiwari/reactnativefbads/NativeAdManager.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Locale;
 
 public class NativeAdManager extends ReactContextBaseJavaModule implements NativeAdsManager.Listener {
   /**
